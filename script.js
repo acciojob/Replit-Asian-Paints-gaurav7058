5153c4b1c7196bd076b9b620537d3b92e1d5f1d8
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-10-15 17:28:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+document.getElementById("Change_button").addEventListener("click",()=>{
+    let blockId=document.getElementById("block_id").value;
+    let colorId=document.getElementById("color_id").value;
+    
+    let gridItem=document.querySelectorAll(".grid-item");
+
+    if(blockId>=1 || blockId<=9){
+        gridItem.forEach(item=>{  
+            item.style.backgroundColor="transparent";
+        })
+        let selectedBlock=document.getElementById(blockId);
+        selectedBlock.style.backgroundColor=colorId;
+        document.getElementById("block_id").value=""
+        document.getElementById("color_id").value=""
+    }
+    else{
+        alert('Please enter a valid Block ID (1-9).');
+    }
+
+      
+})
+document.getElementById("Reset").addEventListener("click",()=>{
+    let gridItem=document.querySelectorAll(".grid-item");
+    gridItem.forEach(item=>{
+        item.style.backgroundColor="transparent";
+    })
+})
