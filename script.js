431d330a565f8e2acfdd10b67ef4f9146f6826db
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-10-15 17:34:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.getElementById("Change_button").addEventListener("click",()=>{
         let selectedBlock=document.getElementById(blockId);
         selectedBlock.style.backgroundColor=colorId;
         document.getElementById("block_id").value=""
-        document.getElementById("color_id").value=""
+        document.getElementById("colour_id").value=""
     }
     else{
         alert('Please enter a valid Block ID (1-9).');
